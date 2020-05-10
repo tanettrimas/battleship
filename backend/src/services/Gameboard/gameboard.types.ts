23f1b32f70e0a80117d66cbca2a24850873f49c1
Tanet Trimas <tanet@trimas.no>
2020-05-11 00:21:37 +0200

    WIP: made gameboard static class

--- backend/src/services/Gameboard/gameboard.types.ts
+++ backend/src/services/Gameboard/gameboard.types.ts
@@ -2,10 +2,3 @@ export interface GameboardCoordinates {
   letter: string,
   number: number
 }
-
-export interface Gameboardes {
-  placeShip(coordinates: GameboardCoordinates[]): boolean
-  recieveAttack(coordinates: GameboardCoordinates): GameboardCoordinates | true
-  isAllShipsSunk(): boolean
-  getBoardRow(letter: string): (string | number)[]
-}
--- backend/src/services/Gameboard/index.ts
+++ backend/src/services/Gameboard/index.ts
@@ -1,32 +1,36 @@
-import { Gameboardes, GameboardCoordinates } from './gameboard.types';
+import { GameboardCoordinates } from './gameboard.types';
 import Ship from '../Ship';
 
-class Gameboard implements Gameboardes {
-  private matrix: Map<string, (number | string)[]> = new Map()
+class Gameboard {
+  private static matrix: Map<string, (number | string)[]> = new Map()
 
-  private MATRIX_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
+  private static MATRIX_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
 
-  private MATRIX_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  private static MATRIX_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  constructor() {
-    this.createMatrix(this.matrix);
-  }
-
-  private createMatrix(matrix: Map<string, (number | string)[]>) {
+  private static createMatrix(matrix: Map<string, (number | string)[]>) {
     this.MATRIX_LETTERS.forEach((letter) => {
       matrix.set(letter, this.MATRIX_NUMBERS);
     });
     this.matrix = matrix;
   }
 
-  getBoardRow(letter: string): (string | number)[] {
+  static createGameboard() {
+    this.createMatrix(this.matrix);
+  }
+
+  static clearGameboard() {
+    this.matrix.clear();
+  }
+
+  static getBoardRow(letter: string): (string | number)[] {
     if (!this.MATRIX_LETTERS.includes(letter)) {
       throw new Error(`Outside of valid range [${this.MATRIX_LETTERS[0]}-${this.MATRIX_LETTERS[this.MATRIX_LETTERS.length - 1]}]`);
     }
     return this.matrix.get(letter)!;
   }
 
-  placeShip(cords: GameboardCoordinates[]): boolean {
+  static placeShip(cords: GameboardCoordinates[]): boolean {
     const numberCoordinates = cords.map((cord) => cord.number);
     // TODO: Fix this hack..
     const shipDirection = cords.every((cord) => cord.letter === cords[0].letter) ? 'vertical' : 'horizontal';
@@ -45,7 +49,7 @@ class Gameboard implements Gameboardes {
     return true;
   }
 
-  recieveAttack(cords: GameboardCoordinates): GameboardCoordinates | true {
+  static recieveAttack(cords: GameboardCoordinates): GameboardCoordinates | true {
     // 1. Get coordinates
     // 2. Find the occurence of the ship based on coordinate
     // 3. If found, place a hit on the ship
@@ -63,15 +67,15 @@ class Gameboard implements Gameboardes {
   }
 
   // eslint-disable-next-line max-len
-  updateBoardState(letter: string, boardRow: (string | number)[], index: number, didHit: boolean): void {
+  static updateBoardState(letter: string, boardRow: (string | number)[], index: number, didHit: boolean): void {
     const newBoard = [...boardRow];
     newBoard[index] = didHit ? 'hit' : 'missed';
     this.matrix.set(letter, newBoard);
   }
 
 
-  isAllShipsSunk(): boolean {
-    throw new Error('Method not implemented.');
+  static isAllShipsSunk(): boolean {
+    return Ship.getStorage().every((ship) => ship.isSunk());
   }
 }
 
